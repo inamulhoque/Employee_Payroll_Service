ead043d41b18ae45a608bfb6851144b93f77b4be
Inamul <inamulhoque097@gmail.com>
2022-07-21 20:10:55 +0530

    Added:till UC11 functionality

--- src/main/java/EmpPayrollMySQL.java
+++ src/main/java/EmpPayrollMySQL.java
@@ -36,4 +36,15 @@ public class EmpPayrollMySQL {
     insert into employee_payroll(name,salary,start_date,gender,phone,address,department,Basic_Pay,Deductions,Taxable_Pay,Income_Tax,Net_Pay) values("Terrisa",4000,"2021-12-01",'F',9854011236,"Guwahati",'Sales',4000,200,1000,300,2700);
     insert into employee_payroll(name,salary,start_date,gender,phone,address,department,Basic_Pay,Deductions,Taxable_Pay,Income_Tax,Net_Pay) values("Terrisa",4000,"2021-12-01",'F',9854011236,"Guwahati",'Sales',4000,200,1000,300,2700);
     select * from employee_payroll where name = "Terrisa";
+    //UC11
+
+    create table Employee_Department(Employee_ID int not null primary key auto_increment, department_Name varchar(30));
+    insert into Employee_Department values(1, "Marketing");
+    insert into Employee_Department values(2, "Sales");
+    select * from Employee_Department;
+    create table emp_Dept (eID int Not Null , Employee_ID int Not Null);
+    insert into emp_dept values(3,1);
+    insert into emp_dept values(3,2);
+    select * from emp_dept;
+    select employee_payroll.name, emp_dept.Employee_ID from employee_payroll inner Join emp_dept ON employee_payroll.id = emp_dept.eID;
 }
