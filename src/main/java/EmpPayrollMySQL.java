52f850a0e62aadb2d2f0f9adec85e9129142fee1
Inamul <inamulhoque097@gmail.com>
2022-07-21 20:06:27 +0530

    Added:till UC11 functionality added

--- src/main/java/EmpPayrollMySQL.java
+++ src/main/java/EmpPayrollMySQL.java
@@ -37,6 +37,7 @@ public class EmpPayrollMySQL {
     insert into employee_payroll(name,salary,start_date,gender,phone,address,department,Basic_Pay,Deductions,Taxable_Pay,Income_Tax,Net_Pay) values("Terrisa",4000,"2021-12-01",'F',9854011236,"Guwahati",'Sales',4000,200,1000,300,2700);
     select * from employee_payroll where name = "Terrisa";
     //UC11
+
     create table Employee_Department(Employee_ID int not null primary key auto_increment, department_Name varchar(30));
     insert into Employee_Department values(1, "Marketing");
     insert into Employee_Department values(2, "Sales");
